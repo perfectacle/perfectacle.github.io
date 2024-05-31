8843d5b0f834aa025deff1bd607cf02599289dd0
perfectacle <perfectacle@users.noreply.github.com>
2024-05-31 13:43:38 +0000

    deploy: 7e5177eb0bd495e87e1f5f8da15f4cb7cec5d6e1

--- js/third-party/fancybox.js
+++ js/third-party/fancybox.js
@@ -1,38 +1,8 @@
-document.addEventListener('page:loaded', () => {
-
-  /**
+/* global Fancybox */
+document.addEventListener("page:loaded",(()=>{
+/**
    * Wrap images with fancybox.
    */
-  document.querySelectorAll('.post-body :not(a) > img, .post-body > img').forEach(element => {
-    const $image = $(element);
-    const imageLink = $image.attr('data-src') || $image.attr('src');
-    const $imageWrapLink = $image.wrap(`<a class="fancybox fancybox.image" href="${imageLink}" itemscope itemtype="http://schema.org/ImageObject" itemprop="url"></a>`).parent('a');
-    if ($image.is('.post-gallery img')) {
-      $imageWrapLink.attr('data-fancybox', 'gallery').attr('rel', 'gallery');
-    } else if ($image.is('.group-picture img')) {
-      $imageWrapLink.attr('data-fancybox', 'group').attr('rel', 'group');
-    } else {
-      $imageWrapLink.attr('data-fancybox', 'default').attr('rel', 'default');
-    }
-
-    const imageTitle = $image.attr('title') || $image.attr('alt');
-    if (imageTitle) {
-      // Do not append image-caption if pandoc has already created a figcaption
-      if (!$imageWrapLink.next('figcaption').length) {
-        $imageWrapLink.append(`<p class="image-caption">${imageTitle}</p>`);
-      }
-      // Make sure img title tag will show correctly in fancybox
-      $imageWrapLink.attr('title', imageTitle).attr('data-caption', imageTitle);
-    }
-  });
-
-  $.fancybox.defaults.hash = false;
-  $('.fancybox').fancybox({
-    loop   : true,
-    helpers: {
-      overlay: {
-        locked: false
-      }
-    }
-  });
-});
+document.querySelectorAll(".post-body :not(a) > img, .post-body > img").forEach((t=>{const e=t.dataset.src||t.src,a=document.createElement("a");a.classList.add("fancybox"),a.href=e,a.setAttribute("itemscope",""),a.setAttribute("itemtype","http://schema.org/ImageObject"),a.setAttribute("itemprop","url");let o="default";null!==t.closest(".post-gallery")?o="gallery":null!==t.closest(".group-picture")&&(o="group"),a.dataset.fancybox=o;const s=t.title||t.alt;s&&(a.title=s,
+// Make sure img captions will show correctly in fancybox
+a.dataset.caption=s),t.wrap(a)})),Fancybox.bind("[data-fancybox]")}));
